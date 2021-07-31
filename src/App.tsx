d282a34c35f48c57568f42bdb60c59b1f8a66810
namnguyen240498 <nam.nguyen@eastplayers.io>
2021-07-31 13:52:52 +0700

    new create

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, {FC} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const App: FC = () => {
+  return (
+    <View>
+      <Text>1</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
