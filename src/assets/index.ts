ef15106f58cffb946ab453f37380eee0d6781799
namnguyen240498 <nam.nguyen@eastplayers.io>
2021-08-09 11:04:56 +0700

    update

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,9 @@
 const Images = {
-  ic_arrow_left: require('./images/arrow-left.png'),
-  ic_arrow_right: require('./images/arrow-right.png'),
-  ic_edit: require('./images/edit-square.png'),
+  ic_arrow_left: require('./images/arrowleft.png'),
+  ic_arrow_right: require('./images/arrowright.png'),
+  ic_edit: require('./images/edit.png'),
   ic_logout: require('./images/logout.png'),
-  ic_close: require('./images/close-square.png'),
+  ic_close: require('./images/close.png'),
   ic_logo: require('./images/logo.png'),
   ic_delete: require('./images/Delete.png'),
 };
